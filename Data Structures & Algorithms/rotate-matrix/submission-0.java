9c89c4eb80076e45a2592b28bb87f6baf89def41
RAJESWAR S <rajesintern@gmail.com>
2026-04-14 22:29:05 +0530

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void rotate(int[][] matrix) 
+    {
+        int n = matrix.length;
+
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = i + 1;j<n;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i = 0;i<n;i++)
+        {
+            reverse(matrix[i]);
+        }
+    }
+
+    private void reverse(int[] row)
+    {
+        int left = 0;
+        int right = row.length - 1;
+
+        while(left < right)
+        {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
